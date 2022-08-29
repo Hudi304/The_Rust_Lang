f9e8aea53086596e7c94160cc3fb4b778df21b00
Hudi304 <hudisteanum@yahoo.com>
2022-08-29 11:53:52 +0300

    Implement guessing game

--- the_rust_lang/gussing_game/src/main.rs
+++ the_rust_lang/gussing_game/src/main.rs
@@ -1,3 +1,44 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn chapter_2_guessing_game() {
+    //! println() is not a function by default, use the macro
+
+    println!("Guess the number!");
+    println!("Please input yout guess.");
+
+    let mut guess = String::new();
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("secret number: {secret_number}");
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    //* parse is interesting
+    //* it parses a variable to another type  */
+    //* the type is chosen by the type annotation of tha variable */
+    let guess: u16 = guess.trim().parse().expect("please type a number!");
+
+    //? "crab pincer operator"
+    println!("You guessed: {guess}");
+    //? there's more than one type of crate
+    //? binary crate
+    //? library crate
+    // ! .lock-files
+    //? it will only update if there are no breaking changes
+    //? if the dependencies has breaking changes it will not update it
+    //* learn more about dependency management */
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    chapter_2_guessing_game()
 }
