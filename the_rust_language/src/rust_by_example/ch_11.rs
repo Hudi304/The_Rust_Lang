27b6dae0301387c01c99781a81221d835c637d19
Hudi304 <hudisteanum@yahoo.com>
2023-03-10 19:12:01 +0200

    Read chapter 11 [Crates]

---
+++ the_rust_language/src/rust_by_example/ch_11.rs
@@ -0,0 +1,12 @@
+
+// CRATE 
+// = compilation unit (and crates are files)
+
+// a crate contains modules 
+// modules do not get compiled individually 
+// only crates get compiled
+
+// a crate can be compiled into 
+    // - a binary 
+    // - library
+    
