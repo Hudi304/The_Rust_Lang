1095392de4d626ce3e3efe77f1968464527ef273
Hudi304 <hudisteanum@yahoo.com>
2023-05-13 20:32:59 +0300

    Add Rectangle test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,13 +2,42 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
