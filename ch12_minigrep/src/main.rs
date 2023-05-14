89163c62488f1d36936db9247272987af004d703
Hudi304 <hudisteanum@yahoo.com>
2023-05-14 16:40:15 +0300

    Complete Ch 12.3 [The Book]

--- ch12_minigrep/src/main.rs
+++ ch12_minigrep/src/main.rs
@@ -1,15 +1,24 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::Config;
 
 fn main() {
+    // responsibility of main.rs :
+    //      Calling the command line parsing logic with the argument values
+    //      Setting up any other configuration
+    //      Calling a run function in lib.rs
+    //      Handling the error if run returns an error
+
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("\nWith text:\n{contents}");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
