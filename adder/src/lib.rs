b0b59eeb4e1d9b9af5abf916808ee3ff6183dbeb
Hudi304 <hudisteanum@yahoo.com>
2023-05-14 14:27:33 +0300

    Complete ch 11.1 [The Book]

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -14,6 +14,45 @@ impl Rectangle {
     }
 }
 
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width < other.width && self.height > other.height
+//     }
+// }
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+// pub fn add_two(a: i32) -> i32 {
+//     a + 3
+// }
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -24,7 +63,7 @@ mod tests {
     }
     #[test]
     fn another() {
-        panic!("Make this test fail");
+        // panic!("Make this test fail");
     }
 
     #[test]
@@ -39,5 +78,54 @@ mod tests {
         };
 
         assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn it_works() -> Result<(), String> {
+            if 2 + 2 == 4 {
+                Ok(())
+            } else {
+                Err(String::from("two plus two does not equal four"))
+            }
+        }
+        // in this kind of code you can use the ? operator to make the code panic
+        // it should be way shorter an easier to read
     }
 }
