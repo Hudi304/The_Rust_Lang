273628eb8146f14d49e9114b7e71a410fc46db33
Hudi304 <hudisteanum@yahoo.com>
2023-05-14 14:45:00 +0300

    Complete Ch 11.2 [The Book]

--- the_rust_language/src/the_book/chapter_11.rs
+++ the_rust_language/src/the_book/chapter_11.rs
@@ -1,3 +1,4 @@
+// see the adder crate in this repository
 pub fn tests() {
     //? set up needed data or state
     //? run the code you want to test
@@ -18,4 +19,42 @@ pub fn tests() {
     // if it returns false, it calls panic!()
 
     // assert_eq! param order does not matter
+
+    ////////////////////////////////
+    /// Controlling How The Tests are Run
+    /// cargo compiles them in test mode and runs the binary
+    
+    // the default behavior is to run all tests in parallel
+    // you can override this behavior with command line options
+
+    // because the  test are run in parallel
+    // you make sure that they do not depend on each other
+
+    // you can pick how many threads you want to allocate
+    // to run the tests with --test-threads
+    //? cargo test -- --test-threads=1
+
+    ////////////////////////////////
+    //  Showing function output
+    //  by default you can not see any !print()
+    // you can override the output with :
+    //? $ cargo test -- --show-output
+
+    ////////////////////////////////
+    // Running a subset of tests by name
+    //? cargo test exploration
+    //?  this does not need to be the full name
+    //? you can write part of the name and cargo will fuzzy find
+
+    ////////////////////////////////
+    // Ignoring tests unless specifically requested
+    // to ignore use : 
+    //?  #[ignore]
+    // to run the ignored tests use: 
+    //? cargo run -- --ignored // just ignored
+    //? cargo run -- --include-ignored // all
+
+
+
+
 }
