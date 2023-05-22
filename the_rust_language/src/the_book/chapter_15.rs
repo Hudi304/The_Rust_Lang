f79bcd8ad4f6d59a65e3fd0ad297d85834642e66
Hudi304 <hudisteanum@yahoo.com>
2023-05-22 17:55:23 +0300

    Complete Ch15.2 [The Book]

--- the_rust_language/src/the_book/chapter_15.rs
+++ the_rust_language/src/the_book/chapter_15.rs
@@ -28,9 +28,64 @@ enum List {
 
 use List::{Cons, Nil};
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+use std::ops::Deref;
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
 pub fn main() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     // let list = Cons(1, Cons(2, Cons(3, Nil)));
     let b = Box::new(5);
     println!("b = {}", b);
+
+    let x = 5;
+    let y = &x;
+    let z = Box::new(x);
+    let k = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    assert_eq!(5, *z);
+    assert_eq!(5, *k);
+
+    // deref coercion
+    let m = MyBox::new(String::from("Rust"));
+    // &MyBox -> &String -> &str
+    hello(&m);
+
+    // this is the same thing, if Rust didn't implement it deref coercion
+    let m = MyBox::new(String::from("Rust"));
+    hello(&(*m)[..]);
+
+    // (*m) -> the value of the box (String)
+    // &(String)[..] -> &str
+
+    //! NO RUNTIME PENALTY, it is all resolved at compile time
+
+    // There is also the DerefMut trait that overrides the behavior 
+    // of the '*' operator on mutable references 
+
+    // Rust does deref coercion for these 3 cases : 
+    // - &T to &U when T:Deref<Target=U>
+    // - &mut T to &mut U when T:DerefMut<Target=U>
+    // - &mut T to & U when T:Deref<Target=U>
+
+
+
 }
