f779ca3b35a711fd1b74d649868a88e39016f833
Hudi304 <hudisteanum@yahoo.com>
2023-05-22 14:27:00 +0300

    Fix errors

--- the_rust_language/src/lib.rs
+++ the_rust_language/src/lib.rs
@@ -1,38 +1,38 @@
-//! # My Crate
-//!
-//! `my_crate` is a collection of utilities to make performing certain
-//! calculations more convenient.
+// //! # My Crate
+// //!
+// //! `my_crate` is a collection of utilities to make performing certain
+// //! calculations more convenient.
 
-/// Adds one to the number given.
-// --snip--
+// /// Adds one to the number given.
+// // --snip--
 
 
-//! # Art
-//!
-//! A library for modeling artistic concepts.
+// //! # Art
+// //!
+// //! A library for modeling artistic concepts.
 
-pub mod kinds {
-  /// The primary colors according to the RYB color model.
-  pub enum PrimaryColor {
-      Red,
-      Yellow,
-      Blue,
-  }
+// pub mod kinds {
+//   /// The primary colors according to the RYB color model.
+//   pub enum PrimaryColor {
+//       Red,
+//       Yellow,
+//       Blue,
+//   }
 
-  /// The secondary colors according to the RYB color model.
-  pub enum SecondaryColor {
-      Orange,
-      Green,
-      Purple,
-  }
-}
+//   /// The secondary colors according to the RYB color model.
+//   pub enum SecondaryColor {
+//       Orange,
+//       Green,
+//       Purple,
+//   }
+// }
 
-pub mod utils {
-  use crate::kinds::*;
+// pub mod utils {
+//   use crate::kinds::*;
 
-  /// Combines two primary colors in equal amounts to create
-  /// a secondary color.
-  pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
-      // --snip--
-  }
-}
+//   /// Combines two primary colors in equal amounts to create
+//   /// a secondary color.
+//   pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+//       // --snip--
+//   }
+// }
--- the_rust_language/src/the_book/chapter_14.rs
+++ the_rust_language/src/the_book/chapter_14.rs
@@ -12,32 +12,32 @@ pub fn add_one(x: i32) -> i32 {
   x + 1
 }
 
-// # Examples
-// # Panics
-// # Errors
-// # Safety
+// // # Examples
+// // # Panics
+// // # Errors
+// // # Safety
 
-use art::kinds::PrimaryColor;
-use art::utils::mix;
+// use art::kinds::PrimaryColor;
+// use art::utils::mix;
 
-fn main() {
-    let red = PrimaryColor::Red;
-    let yellow = PrimaryColor::Yellow;
-    mix(red, yellow);
-}
+// fn main() {
+//     let red = PrimaryColor::Red;
+//     let yellow = PrimaryColor::Yellow;
+//     mix(red, yellow);
+// }
 
-//! # Art
-//!
-//! A library for modeling artistic concepts.
+// //! # Art
+// //!
+// //! A library for modeling artistic concepts.
 
-pub use self::kinds::PrimaryColor;
-pub use self::kinds::SecondaryColor;
-pub use self::utils::mix;
+// pub use self::kinds::PrimaryColor;
+// pub use self::kinds::SecondaryColor;
+// pub use self::utils::mix;
 
-pub mod kinds {
-    // --snip--
-}
+// pub mod kinds {
+//     // --snip--
+// }
 
-pub mod utils {
-    // --snip--
-}
+// pub mod utils {
+//     // --snip--
+// }
