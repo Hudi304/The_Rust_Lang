6e73c00654b520208fba80813b3f403441944267
Hudi304 <hudisteanum@yahoo.com>
2023-06-02 15:14:32 +0300

    Complete Ch15.6 [The Book] Memory leaks

--- the_rust_language/src/the_book/chapter_15.rs
+++ the_rust_language/src/the_book/chapter_15.rs
@@ -258,29 +258,125 @@ mod tests {
     }
 }
 
-use std::cell::RefCell;
-use std::rc::Rc;
+// use std::cell::RefCell;
+// use std::rc::Rc;
 
-#[derive(Debug)]
-enum ListMut {
-    Cons(Rc<RefCell<i32>>, Rc<List>),
-    Nil,
-}
+// #[derive(Debug)]
+// enum ListMut {
+//     Cons(Rc<RefCell<i32>>, Rc<List>),
+//     Nil,
+// }
 
+// fn multiple_owner_interior_mutability() {
+//     let value = Rc::new(RefCell::new(5));
 
-fn multiple_owner_interior_mutability() {
-    let value = Rc::new(RefCell::new(5));
+//     let a = Rc::new(ListMut::Cons(Rc::clone(&value), Rc::new(Nil)));
 
-    let a = Rc::new(ListMut::Cons(Rc::clone(&value), Rc::new(Nil)));
+//     let b = ListMut::Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+//     let c = ListMut::Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
 
-    let b = ListMut::Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
-    let c = ListMut::Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+//     *value.borrow_mut() += 10;
 
-    *value.borrow_mut() += 10;
+//     println!("a after = {:?}", a);
+//     println!("b after = {:?}", b);
+//     println!("c after = {:?}", c);
+// }
 
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+// fn memory_leaks() {
+//     // memory that is never cleaned up -> memory leak
+
+//     use std::cell::RefCell;
+//     use std::rc::Rc;
+//     use List::{Cons, Nil};
+
+//     #[derive(Debug)]
+//     enum List {
+//         Cons(i32, RefCell<Rc<List>>),
+//         Nil,
+//     }
+
+//     impl List {
+//         fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+//             match self {
+//                 Cons(_, item) => Some(item),
+//                 Nil => None,
+//             }
+//         }
+//     }
+//     let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+//     println!("a initial rc count = {}", Rc::strong_count(&a));
+//     println!("a next item = {:?}", a.tail());
+
+//     let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+//     println!("a rc count after b creation = {}", Rc::strong_count(&a));
+//     println!("b initial rc count = {}", Rc::strong_count(&b));
+//     println!("b next item = {:?}", b.tail());
+
+//     if let Some(link) = a.tail() {
+//         *link.borrow_mut() = Rc::clone(&b);
+//     }
+
+//     println!("b rc count after changing a = {}", Rc::strong_count(&b));
+//     println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+//     // Uncomment the next line to see that we have a cycle;
+//     // it will overflow the stack
+//     // println!("a next item = {:?}", a.tail());
+// }
+
+fn tree() {
+    use std::cell::RefCell;
+    use std::rc::{Rc, Weak};
+
+    #[derive(Debug)]
+    struct Node {
+        value: i32,
+        parent: RefCell<Weak<Node>>,
+        children: RefCell<Vec<Rc<Node>>>,
+    }
+
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+
+    {
+        let branch = Rc::new(Node {
+            value: 5,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
+
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
 
 pub fn main() {
@@ -301,5 +397,7 @@ pub fn main() {
     // drop_trait();
     // Rc_type();
     // ref_count();
-    multiple_owner_interior_mutability();
+    // multiple_owner_interior_mutability();
+    // memory_leaks();
+    tree();
 }
