afe9f6fb91aa8b164cf05896cabf5e468f04b526
Hudi304 <hudisteanum@yahoo.com>
2023-06-02 14:48:02 +0300

    Complete Ch15.5 [The Book] Interior Mutability

--- the_rust_language/src/the_book/chapter_15.rs
+++ the_rust_language/src/the_book/chapter_15.rs
@@ -152,6 +152,137 @@ fn ref_count() {
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
+// interior mutability design pattern
+// allows you to mutate data even if it has
+// immutable references
+// this is unsafe
+
+// the unsafe code is wrapped into a safe API
+
+// the difference between Box<T> and RefCell<T>
+// is that the borrow rules are enforced at compile time
+// in the cas of Box<T>
+// and at runtime in the cas of RefCell<T>
+
+// a.k.a. if you break the rules the program will panic
+
+//? do some research on the HaltingProblem
+
+// recap:
+// Rc<T> enables multiple owners of the same data
+// Box<T> and RefCell<T> have single owners
+
+// Box<T> allows mutable or immutable references at compile time
+// Rc<T> allows only immutable borrows at compile time
+// RefCell<T> allows mutable or immutable borrows at runtime
+
+// Because RefCell<T> allows mutable borrows, you can mutate
+// the value even when it is immutable
+
+fn borrow_mutable() {
+    let x = 5;
+    // let y = &mut x;
+}
+
+fn limit_tracker_example() {
+    pub trait Messenger {
+        fn send(&self, msg: &str);
+    }
+
+    pub struct LimitTracker<'a, T: Messenger> {
+        messenger: &'a T,
+        value: usize,
+        max: usize,
+    }
+
+    impl<'a, T> LimitTracker<'a, T>
+    where
+        T: Messenger,
+    {
+        pub fn new(messenger: &'a T, max: usize) -> LimitTracker<'a, T> {
+            LimitTracker {
+                messenger,
+                value: 0,
+                max,
+            }
+        }
+
+        pub fn set_value(&mut self, value: usize) {
+            self.value = value;
+
+            let percentage_of_max = self.value as f64 / self.max as f64;
+
+            if percentage_of_max >= 1.0 {
+                self.messenger.send("Error: You are over your quota!");
+            } else if percentage_of_max >= 0.9 {
+                self.messenger
+                    .send("Urgent warning: You've used up over 90% of your quota!");
+            } else if percentage_of_max >= 0.75 {
+                self.messenger
+                    .send("Warning: You've used up over 75% of your quota!");
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum ListMut {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+
+fn multiple_owner_interior_mutability() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(ListMut::Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = ListMut::Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = ListMut::Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
+
 pub fn main() {
     // (*m) -> the value of the box (String)
     // &(String)[..] -> &str
@@ -169,5 +300,6 @@ pub fn main() {
     // cons_list();
     // drop_trait();
     // Rc_type();
-    ref_count();
+    // ref_count();
+    multiple_owner_interior_mutability();
 }
