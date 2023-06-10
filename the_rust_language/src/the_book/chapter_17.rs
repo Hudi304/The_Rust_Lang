a18fa9ad391e58ec885e9cefb926390cbd7970b7
Hudi304 <hudisteanum@yahoo.com>
2023-06-10 14:49:58 +0300

    Complete Ch17.3 [The Book] O.O.P. Design patterns

--- the_rust_language/src/the_book/chapter_17.rs
+++ the_rust_language/src/the_book/chapter_17.rs
@@ -159,9 +159,8 @@ fn ch_17_2() {
 
     screen.run();
 
-
-    // in the case of generic types the compiler generates non generic implementation 
-    // of functions for each each concrete type 
+    // in the case of generic types the compiler generates non generic implementation
+    // of functions for each each concrete type
     // the code that results from this is called static dispatch
     // this is opposed to dynamic dispatch, we'll figure out at runtime witch method to call
 
@@ -169,7 +168,187 @@ fn ch_17_2() {
     // but because it makes the code much more flexible it is a cost to consider
 }
 
+fn ch_17_3_oop_way() {
+    //? OOP DESIGN PATTERNS
+
+    //* THE STATE PATTERN */
+    //? The final functionality will look like this:
+    //      A blog post starts as an empty draft.
+    //      When the draft is done, a review of the post is requested.
+    //      When the post is approved, it gets published.
+    //      Only published blog posts return content to print,
+    //          so unapproved posts can’t accidentally be published.
+
+    pub struct Post {
+        state: Option<Box<dyn State>>,
+        content: String,
+    }
+
+    impl Post {
+        pub fn new() -> Post {
+            Post {
+                state: Some(Box::new(Draft {})),
+                content: String::new(),
+            }
+        }
+
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+
+        pub fn content(&self) -> &str {
+            self.state.as_ref().unwrap().content(self)
+        }
+
+        pub fn request_review(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.request_review())
+            }
+        }
+
+        pub fn approve(&mut self) {
+            if let Some(s) = self.state.take() {
+                self.state = Some(s.approve())
+            }
+        }
+    }
+
+    trait State {
+        fn request_review(self: Box<Self>) -> Box<dyn State>;
+
+        fn approve(self: Box<Self>) -> Box<dyn State>;
+
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            ""
+        }
+    }
+
+    struct Draft {}
+
+    impl State for Draft {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            Box::new(PendingReview {})
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+    }
+
+    struct PendingReview {}
+
+    impl State for PendingReview {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            Box::new(Published {})
+        }
+    }
+
+    struct Published {}
+
+    impl State for Published {
+        fn request_review(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        fn approve(self: Box<Self>) -> Box<dyn State> {
+            self
+        }
+
+        // we take a post as a parameter and return part of it
+        // so their lifetimes are entangled
+        fn content<'a>(&self, post: &'a Post) -> &'a str {
+            &post.content
+        }
+    }
+
+    // the rules live in the state object instead of being scattered throughout Post
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+}
+
+fn ch_17_3_the_rusty_way() {
+    pub struct Post {
+        content: String,
+    }
+
+    pub struct DraftPost {
+        content: String,
+    }
+
+    impl Post {
+        pub fn new() -> DraftPost {
+            DraftPost {
+                content: String::new(),
+            }
+        }
+
+        pub fn content(&self) -> &str {
+            &self.content
+        }
+    }
+
+    impl DraftPost {
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+
+        // it takes ownership of the Post they are called on and consume it
+        // this way we don't have any lingering DraftPost
+        pub fn request_review(self) -> PendingReviewPost {
+            PendingReviewPost {
+                content: self.content,
+            }
+        }
+    }
+
+    pub struct PendingReviewPost {
+        content: String,
+    }
+
+    impl PendingReviewPost {
+        // it takes ownership of the Post they are called on and consume it
+        // this way we don't have any lingering PendingReviewPost
+        pub fn approve(self) -> Post {
+            Post {
+                content: self.content,
+            }
+        }
+    }
+
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
+    
+    // this does not quite follow the oop design pattern because the transformations 
+    // between states are no longer encapsulated
+
+    // the big + is that invalid states are now impossible
+    // this ensures that the display of an unapproved post will be discovered before production
+     
+   
+}
+
 pub fn main() {
     // ch_17_1();
-    ch_17_2();
+    // ch_17_2();
+    ch_17_3_oop_way();
 }
