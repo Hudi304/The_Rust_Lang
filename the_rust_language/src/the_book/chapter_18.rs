13aa7379973552738093820f81c58dd398c73af5
Hudi304 <hudisteanum@yahoo.com>
2023-06-11 23:15:25 +0300

    Complete Ch18.3 [The Book] Pattern Syntax

--- the_rust_language/src/the_book/chapter_18.rs
+++ the_rust_language/src/the_book/chapter_18.rs
@@ -113,7 +113,7 @@ fn ch_18_2() {
     // warn you about irrefutable patterns because they don't
     // really make sense in context
 
-    //? this does not compile 
+    //? this does not compile
     let some_option_value: Option<i32> = None;
     // let Some(x) = some_option_value;
 
@@ -126,8 +126,306 @@ fn ch_18_2() {
     };
 }
 
+fn ch_18_3() {
+    fn simple_match() {
+        let x = 1;
+
+        match x {
+            1 => println!("one"),
+            2 => println!("two"),
+            3 => println!("three"),
+            _ => println!("anything"),
+        }
+    }
+
+    fn shadow_match() {
+        let x = Some(5);
+        let y = 10;
+
+        match x {
+            //this does not match
+            Some(50) => println!("Got 50"),
+            // this y is a shadow of y, and does not mean 10
+            // it means any value
+            // this is because this is a new scope
+            Some(y) => println!("Matched, y = {y}"),
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        println!("at the end: x = {:?}, y = {y}", x);
+    }
+
+    fn or_match() {
+        let x = 1;
+
+        match x {
+            1 | 2 => println!("one or two"),
+            3 => println!("three"),
+            _ => println!("anything"),
+        }
+    }
+
+    fn range_match() {
+        let x = 5;
+
+        match x {
+            1..=5 => println!("one through five"),
+            _ => println!("something else"),
+        }
+    }
+
+    fn char_range() {
+        let x = 'c';
+
+        match x {
+            'a'..='j' => println!("early ASCII letter"),
+            'k'..='z' => println!("late ASCII letter"),
+            _ => println!("something else"),
+        }
+    }
+
+    fn destructure() {
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        let p = Point { x: 0, y: 7 };
+
+        let Point { x: a, y: b } = p;
+        // these are the same
+        // let Point { x, y } = p;
+        assert_eq!(0, a);
+        assert_eq!(7, b);
+
+        match p {
+            // the match statement stops after a pattern matches
+            // to {0,0} would be on the X axis here
+            Point { x, y: 0 } => println!("On the x axis at {x}"),
+            Point { x: 0, y } => println!("On the y axis at {y}"),
+            Point { x, y } => {
+                println!("On neither axis: ({x}, {y})");
+            }
+        }
+
+        let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+    }
+
+    fn enum_destructure() {
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(i32, i32, i32),
+        }
+
+        let msg = Message::ChangeColor(0, 160, 255);
+
+        match msg {
+            Message::Quit => {
+                println!("The Quit variant has no data to destructure.");
+            }
+            Message::Move { x, y } => {
+                println!("Move in the x direction {x} and in the y direction {y}");
+            }
+            Message::Write(text) => {
+                println!("Text message: {text}");
+            }
+            Message::ChangeColor(r, g, b) => {
+                println!("Change the color to red {r}, green {g}, and blue {b}",)
+            }
+        }
+    }
+
+    fn nested_structures() {
+        enum Color {
+            Rgb(i32, i32, i32),
+            Hsv(i32, i32, i32),
+        }
+
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(Color),
+        }
+
+        let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+        match msg {
+            Message::ChangeColor(Color::Rgb(r, g, b)) => {
+                println!("Change color to red {r}, green {g}, and blue {b}");
+            }
+            Message::ChangeColor(Color::Hsv(h, s, v)) => {
+                println!("Change color to hue {h}, saturation {s}, value {v}")
+            }
+            _ => (),
+        }
+    }
+
+    fn ignore_values() {
+        //? ignore a parameter
+        fn foo(_: i32, y: i32) {
+            println!("This code only uses the y parameter: {}", y);
+        }
+
+        foo(3, 4);
+
+        let mut setting_value = Some(5);
+        let new_setting_value = Some(10);
+
+        match (setting_value, new_setting_value) {
+            (Some(_), Some(_)) => {
+                println!("Can't overwrite an existing customized value");
+            }
+            _ => {
+                setting_value = new_setting_value;
+            }
+        }
+
+        println!("setting is {:?}", setting_value);
+
+        let numbers = (2, 4, 8, 16, 32);
+
+        match numbers {
+            (first, _, third, _, fifth) => {
+                println!("Some numbers: {first}, {third}, {fifth}")
+            }
+        }
+
+        {
+            // there is a difference between using '_'
+            //  and starting the name of the variable with '_'
+            // "_" matches everything but does not bind
+            // "_s" matches everything but it is still a variable and binds
+            let s = Some(String::from("Hello!"));
+            // s is move into _s here
+            if let Some(_s) = s {
+                println!("found a string");
+            }
+
+            // we are trying to use a variable that has been moved
+            // println!("{:?}", s);
+        }
+
+        {
+            let s = Some(String::from("Hello!"));
+
+            if let Some(_) = s {
+                println!("found a string");
+            }
+            println!("{:?}", s);
+            // this works just fine
+            // because the was no binding
+        }
+
+        struct Point {
+            x: i32,
+            y: i32,
+            z: i32,
+        }
+
+        let origin = Point { x: 0, y: 0, z: 0 };
+
+        match origin {
+            // this looks a lot like lisp for some reason
+            Point { x, .. } => println!("x is {}", x),
+        }
+        let numbers = (2, 4, 8, 16, 32);
+
+        match numbers {
+            (first, .., last) => {
+                println!("Some numbers: {first}, {last}");
+            }
+        }
+
+        let numbers = (2, 4, 8, 16, 32);
+
+        //? this is ambiguous and will not compile
+        // match numbers {
+        //     (.., second, ..) => {
+        //         println!("Some numbers: {}", second)
+        //     },
+        // }
+    }
+
+    fn match_guards() {
+        let num = Some(4);
+
+        // ! the compiler can no longer check for exhaustiveness when match guards are used
+        match num {
+            Some(x) if x % 2 == 0 => println!("The number {} is even", x),
+            Some(x) => println!("The number {} is odd", x),
+            None => (),
+        }
+    }
+
+    fn fixing_shadowing_with_guards() {
+        let x = Some(5);
+        let y = 10;
+
+        match x {
+            Some(50) => println!("Got 50"),
+
+            // if n == y is not a pattern, so it does not introduce new shadowing
+            Some(n) if n == y => println!("Matched, n = {n}"),
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        println!("at the end: x = {:?}, y = {y}", x);
+
+        let x = 4;
+        let y = false;
+
+        match x {
+            // if y applies to 4,5 and 6
+            4 | 5 | 6 if y => println!("yes"),
+            _ => println!("no"),
+        }
+    }
+
+    fn bindings() {
+        enum Message {
+            Hello { id: i32 },
+        }
+
+        let msg = Message::Hello { id: 3 };
+
+        match msg {
+            Message::Hello {
+                // rename id if it is in the interval [3,7]
+                id: id_variable @ 3..=7,
+                // test and capture the variable
+            } => println!("Found an id in range: {}", id_variable),
+            Message::Hello { id: 10..=12 } => {
+                // println!("Found an id in range: {}", id);
+                // ! can not find name id
+                //? the value was not captured but the interval can match and the code 
+                //? can be executed 
+                println!("Found an id in another range")
+            }
+            // because any value can match this pattern we can use it 
+            // there are no tests done on it 
+            Message::Hello { id } => println!("Found some other id: {}", id),
+        }
+    }
+
+    // simple_match();
+    // shadow_match();
+    // or_match();
+    // range_match();
+    // char_range();
+    // destructure();
+    // enum_destructure();
+    // ignore_values();
+    // match_guards();
+    // fixing_shadowing_with_guards();
+    bindings();
+}
+
 pub fn main() {
     println!("CHAPTER 18");
     // ch_18_1();
-    ch_18_2();
+    // ch_18_2();
+    ch_18_3();
 }
