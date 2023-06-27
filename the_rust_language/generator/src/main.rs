1f59d78dea21a2d26880251c26f02387b549f47c
Hudi304 <hudisteanum@yahoo.com>
2023-06-27 10:24:22 +0300

    Parse top level response

--- the_rust_language/generator/src/main.rs
+++ the_rust_language/generator/src/main.rs
@@ -1,6 +1,7 @@
 use reqwest::blocking::get;
 use serde::{self, Deserialize, Serialize};
-use std::fs;
+use serde_json::Value::Object;
+use std::{fs, path};
 #[derive(Serialize, Deserialize, Debug)]
 struct LocalFileFormat<'a> {
     swagger_url: &'a str,
@@ -17,9 +18,49 @@ struct ResponseInfo {
 struct SwaggerFormat {
     pub openapi: String,
     pub info: ResponseInfo,
-    pub paths: serde_json::Value,      // components: String,
-    pub components: serde_json::Value, // components: String,
+    pub paths: serde_json::Value,
+    pub components: SwaggerComponents,
 }
+#[derive(Serialize, Deserialize, Debug)]
+struct SwaggerComponents {
+    schemas: serde_json::Value,
+}
+
+#[derive(Deserialize)]
+struct SwaggerSchema {
+    schema_type: String,
+    //TODO check if you can add a type for the value
+    properties: serde_json::Value,
+    additionalProperties: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SchemaProperty {
+    property_type: String,
+    nullable: bool,
+}
+
+struct ModelSchema {
+    schema_type: String,
+    //TODO check if you can add a type for the value
+    properties: serde_json::Value,
+    additionalProperties: bool,
+}
+
+struct EnumSchema {
+    enum_values: Vec<String>,
+    enum_type: String,
+}
+
+// TODO try implementing Debug for external struct
+// impl fmt::Debug for serde_json::Map<K, V> {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+//         for (key, value) in self.into_iter() {
+//             write!(f, "[{} : {}]", key, value)
+//         }
+//         return fmt::Result;
+//     }
+// }
 
 fn main() {
     let settings_path = "./local.json";
@@ -45,7 +86,28 @@ fn main() {
     let response_body: SwaggerFormat = serde_json::from_str(&response_body[..])
         .expect("Could not parse api call response body to json");
 
-    println!("{:?}", response_body.info);
+    // println!("{:?}", response_body.info);
+
+    let schemas = response_body.components.schemas;
+    let paths = response_body.paths;
+
+    let paths: serde_json::Map<String, serde_json::Value> = match paths {
+        serde_json::Value::Object(map) => map,
+        _ => panic!(""),
+    };
+
+    let schemas: serde_json::Map<String, serde_json::Value> = match schemas {
+        serde_json::Value::Object(map) => map,
+        _ => panic!(""),
+    };
+
+    // for (schema_name, schema_value) in schemas.into_iter() {
+    //     println!("{:?}", schema_name);
+    // }
+
+    for (path_name, path_value) in paths.into_iter() {
+        println!("{:?}", path_name);
+    }
 
     println!("Hello, world!");
 }
